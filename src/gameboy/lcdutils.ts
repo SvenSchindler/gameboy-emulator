f56f84027959e3b30e07e4d3a73558edf8176f9a
Sven Schindler <svenschindler@192-168-1-118.tpgi.com.au>
2025-01-28 17:49:54 +1100

    add some more display blurriness

--- src/gameboy/lcdutils.ts
+++ src/gameboy/lcdutils.ts
@@ -13,10 +13,26 @@ export class LcdUtils {
   static drawPixelWithDecay(canvasData: ImageData, canvasWidth: number, x: number, y: number, color: RGBA) {
     var index = (x + y * canvasWidth) * 4;
 
-    const decayFactor = 0.49;
+    const decayFactor = 0.65;
     canvasData.data[index + 0] = (1 - decayFactor) * color[0] + decayFactor * canvasData.data[index + 0];
     canvasData.data[index + 1] = (1 - decayFactor) * color[1] + decayFactor * canvasData.data[index + 1];
     canvasData.data[index + 2] = (1 - decayFactor) * color[2] + decayFactor * canvasData.data[index + 2];
-    canvasData.data[index + 3] = (1 - decayFactor) * color[3] + decayFactor * canvasData.data[index + 3];
+
+    // Just to avoid some rounding issues
+    const distance = 3;
+    if (Math.abs(canvasData.data[index + 0] - color[0]) < distance) {
+      canvasData.data[index + 0] = color[0];
+    }
+
+    if (Math.abs(canvasData.data[index + 1] - color[1]) < distance) {
+      canvasData.data[index + 1] = color[1];
+    }
+
+    if (Math.abs(canvasData.data[index + 2] - color[2]) < distance) {
+      canvasData.data[index + 2] = color[2];
+    }
+
+    // We're not dealing with any alpha values
+    canvasData.data[index + 3] = 255;
   }
 }
